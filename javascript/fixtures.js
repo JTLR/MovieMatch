e893fbaaa4b369641d4c3a3a9781245bb85ed465
JTLR <jm_taylor@live.co.uk>
2014-03-20 21:45:33 +0000

    Move fixture data to separate file and add models for year, genre and person

---
+++ javascript/fixtures.js
@@ -0,0 +1,123 @@
+// Fixture data
+
+// Movie
+Moviematch.Movie.FIXTURES = [
+	{
+		id: 1,
+		title: 'The Wolf of Wall Street',
+		year: 2,
+		genres: [1, 2, 3],
+		cast: [1, 4, 5],
+		director: 9,
+		synopsis: 'Based on the true story of Jordan Belfort, from his rise to a wealthy stockbroker living the high life to his fall involving crime, corruption and the federal government.',
+		imgUrl: 'images/the-wolf-of-wall-street.jpg'
+	},
+	{
+		id: 2,
+		title: 'Titanic',
+		year: 1,
+		genres: [4, 5],
+		cast: [1, 2, 3],
+		director: 10,
+		synopsis: 'A seventeen-year-old aristocrat, expecting to be married to a rich claimant by her mother, falls in love with a kind but poor artist aboard the luxurious, ill-fated R.M.S. Titanic.',
+		imgUrl: 'images/titsnic.jpg'
+	},
+	{
+		id: 3,
+		title: '12 Years a Slave',
+		year: 2,
+		genres: [1, 4, 6],
+		cast: [6, 7, 8],
+		director: 'Steve McQueen',
+		synopsis: 'In the antebellum United States, Solomon Northup, a free black man from upstate New York, is abducted and sold into slavery.',
+		imgUrl: 'images/12-years-a-slave.jpg'
+	}
+];
+
+// Year
+Moviematch.Year.FIXTURES = [
+	{
+		id: 1,
+		year: 1997
+	},
+	{
+		id: 2,
+		year: 2013
+	}
+];
+
+// Genre
+Moviematch.Genre.FIXTURES = [
+	{
+		id: 1,
+		genre: 'Biography'
+	},
+	{
+		id: 2,
+		genre: 'Comedy'
+	},
+	{
+		id: 3,
+		genre: 'Crime'
+	},
+	{
+		id: 4,
+		genre: 'Drama'
+	},
+	{
+		id: 5,
+		genre: 'Romance'
+	},
+	{
+		id: 6,
+		genre: 'History'
+	}
+];
+
+// Person
+Moviematch.Person.FIXTURES = [
+	{
+		id: 1,
+		name: 'Leonardo DiCaprio'
+	},
+	{
+		id: 2,
+		name: 'Kate Winslet'
+	},
+	{
+		id: 3,
+		name: 'Billy Zane'
+	},
+	{
+		id: 4,
+		name: 'Jonah Hill'
+	},
+	{
+		id: 5,
+		name: 'Margot Robbie'
+	},
+	{
+		id: 6,
+		name: 'Chiwetel Ejiofor'
+	},
+	{
+		id: 7,
+		name: 'Michael K. Williams'
+	},
+	{
+		id: 8,
+		name: 'Michael Fassbender'
+	},
+	{
+		id: 9,
+		name: 'Martin Scorsese'
+	},
+	{
+		id: 10,
+		name: 'James Cameron'
+	},
+	{
+		id: 11,
+		name: 'Steve McQueen'
+	},
+]
--- javascript/javascript.js
+++ javascript/javascript.js
@@ -3,6 +3,10 @@ window.Moviematch = Ember.Application.create();
 
 Moviematch.ApplicationAdapter = DS.FixtureAdapter.extend();
 
+ENV = {
+    EXPERIMENTAL_CONTROL_HELPER: true
+};
+
 // Routers
 Moviematch.Router.map(function() {
 	this.resource('moviematch', { path: '/' });
@@ -10,7 +14,7 @@ Moviematch.Router.map(function() {
 
 Moviematch.MoviematchRoute = Ember.Route.extend({
 	model: function() {
-		return this.store.find('movie', 3);
+		return this.store.find('movie', 2);
 	},
 	setupController: function(controller, model) {
 		controller.set('movie', model);
@@ -22,51 +26,43 @@ Moviematch.MoviematchRoute = Ember.Route.extend({
 // Movie
 Moviematch.Movie = DS.Model.extend({
 	title: DS.attr('string'),
-	year: DS.attr('number'),
-	genres: DS.attr('array'),
-	cast: DS.attr('array'),
-	director: DS.attr('string'),
+	year: DS.belongsTo('year'),
+	genre: DS.hasMany('genre', {async: true}),
+	cast: DS.hasMany('person', {async: true}),
+	director: DS.belongsTo('person'),
 	synopsis: DS.attr('string'),
 	imgUrl: DS.attr('string')
 });
 
-Moviematch.Movie.FIXTURES = [
-	{
-		id: 1,
-		title: 'The Wolf of Wall Street',
-		year: 2013,
-		genres: ['Biography', 'Comedy', 'Crime'],
-		cast: ['Leonardo DiCaprio', 'Jonah Hill', 'Margot Robbie'],
-		director: 'Martin Scorsese',
-		synopsis: 'Based on the true story of Jordan Belfort, from his rise to a wealthy stockbroker living the high life to his fall involving crime, corruption and the federal government.',
-		imgUrl: 'images/the-wolf-of-wall-street.jpg'
-	},
-	{
-		id: 2,
-		title: 'Titanic',
-		year: 1997,
-		genres: ['Drama', 'Romance'],
-		cast: ['Leonardo DiCaprio', 'Kate Winslet', 'Billy Zane'],
-		director: 'James Cameron',
-		synopsis: 'A seventeen-year-old aristocrat, expecting to be married to a rich claimant by her mother, falls in love with a kind but poor artist aboard the luxurious, ill-fated R.M.S. Titanic.',
-		imgUrl: 'images/titsnic.jpg'
-	},
-	{
-		id: 3,
-		title: '12 Years a Slave',
-		year: 2013,
-		genres: ['Biography', 'Drama', 'History'],
-		cast: ['Chiwetel Ejiofor', 'Michael K. Williams', 'Michael Fassbender'],
-		director: 'Steve McQueen',
-		synopsis: 'In the antebellum United States, Solomon Northup, a free black man from upstate New York, is abducted and sold into slavery.',
-		imgUrl: 'images/12-years-a-slave.jpg'
-	}
-];
+// Year
+Moviematch.Year = DS.Model.extend({
+	year: DS.attr('number'),
+	movie: DS.hasMany('movie')
+});
+
+// Genre
+Moviematch.Genre = DS.Model.extend({
+	genre: DS.attr('string'),
+	movie: DS.hasMany('movie')	
+});
+
+// Person
+Moviematch.Person = DS.Model.extend({
+	name: DS.attr('string'),
+	movie: DS.hasMany('movie')	
+});
+
 
 // Controllers
 Moviematch.MoviematchController = Ember.ArrayController.extend();
 
 Moviematch.MovieController = Ember.ObjectController.extend();
+
+Moviematch.YearController = Ember.ObjectController.extend();
+
+Moviematch.GenreController = Ember.ObjectController.extend();
+
+Moviematch.PersonController = Ember.ObjectController.extend();
 
 // Presentational JS
 $(document).ready(function () {
