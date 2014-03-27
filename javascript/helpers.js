e3fbabd0d3fdf198517ba556f6c9313fe5ebedc0
Joe Taylor <joseph@emosaic.co.uk>
2014-03-27 19:07:46 +0000

    Clean up ifcond helper

--- javascript/helpers.js
+++ javascript/helpers.js
@@ -1,16 +1,12 @@
-
 var counter = 1;
+var $return;
 
 Handlebars.registerHelper('ifCond', function(v1, options) {
-
-
-  if(counter % v1 == 0) {
-  	counter++;
-    return options.fn(this);
-  } else {
-counter++;
-
-  return options.inverse(this);
-}
-
+	if(counter % v1 == 0) {
+		$return = options.fn(this);
+	} else {
+  		$return = options.inverse(this);
+	}
+	counter++;
+	return $return;
 });
