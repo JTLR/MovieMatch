659104e17bb5ea2edbd269bb15e6c7a582264585
JTLR <jm_taylor@live.co.uk>
2014-03-28 22:48:20 +0000

    Pull film data through MovieDB API

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,11 +1,24 @@
 // Application JS
-window.Moviematch = Ember.Application.create();
+var Moviematch = Ember.Application.create();
 
-Moviematch.ApplicationAdapter = DS.FixtureAdapter.extend();
+// Record array
+Moviematch.RecordArray = Ember.ArrayProxy.extend({
 
-ENV = {
-    EXPERIMENTAL_CONTROL_HELPER: true
-};
+});
+
+function createOrGetRecordArray(context) {
+	var array = context._recordArray;
+	if (array) {
+		return array;
+	}
+
+	context._recordArray = array = Moviematch.RecordArray.create();
+
+	array.set('isLoaded', false);
+	array.set('_ids', Ember.Map.create());
+
+	return array;
+}
 
 // Routers
 Moviematch.Router.map(function() {
@@ -14,14 +27,70 @@ Moviematch.Router.map(function() {
 
 Moviematch.MoviematchRoute = Ember.Route.extend({
 	model: function() {
-		return this.store.find('movie');
-	},
-	setupController: function(controller, model) {
-		controller.set('movie', model);
+		return Moviematch.Movie.findAll();
 	}
 });
 
 // Models
+
+Moviematch.Model = Ember.Object.extend({
+
+});
+
+Moviematch.Model.reopenClass({
+	findAll: function() {
+		var array = createOrGetRecordArray(this);
+		this.fetch(array);
+		return array;
+	},
+	fetch: function(array) {
+		var model = this;
+
+		ajax(searchUrl).then(function(data) {
+			model.materializeData(data.results, array.get('_ids'), array);
+		}).then(null, function(err) {
+			console.log(err.message);
+			console.log(err.stack);
+			throw err;
+		});
+	},
+	materializeData: function(data, cache, records) {
+		var model = this;
+
+		var content = data.map(function(item) {
+			var record;
+
+			if (cache.has(item.id)) {
+				record = cache.get(item.id);
+			} else {
+				record = model.create();
+				cache.set(item.id, record);
+			}
+			record.setProperties(item);
+			return record;
+		});
+
+		records.set('content', content);
+		records.set('isLoaded', true);
+	}
+});
+
+var searchUrl = "https://api.themoviedb.org/3/movie/upcoming";
+var apiKey = "30b6b095d358b4ba9eeb5154fdfce80c";
+
+function ajax(url) {
+	return Ember.Deferred.promise(function(promise) {
+		var xhr = $.ajax(url, {
+			data: { api_key: apiKey, page: 1 },
+			dataType: 'jsonp'
+		});
+		xhr.then(function(data) {
+			Ember.run(promise, promise.resolve, data);
+		}, function(err) {
+			promise.reject(e);
+		});
+	});
+}
 
 // User
 Moviematch.User = DS.Model.extend({
@@ -35,16 +104,12 @@ Moviematch.User = DS.Model.extend({
 });
 
 // Movie
-Moviematch.Movie = DS.Model.extend({
-	title: DS.attr('string'),
-	year: DS.belongsTo('year'),
-	genre: DS.hasMany('genre', {async: true}),
-	cast: DS.hasMany('person', {async: true}),
-	director: DS.belongsTo('person'),
-	synopsis: DS.attr('string'),
-	imgUrl: DS.attr('string'),
-    likedBy: DS.hasMany('user', {async: true}),
-    dislikedBy: DS.hasMany('user', {async: true})
+Moviematch.Movie = Moviematch.Model.extend({
+	title: null,
+	img: function() {
+		var posterBasePath = 'https://image.tmdb.org/t/p/original';
+		return posterBasePath + this.get('poster_path');
+	}.property('poster_path')
 });
 
 // Year
